4f6823c4303f33afd15fa9ed0f3690dd39ac4dd4
Matty <m.christopher@hotmail.co.uk>
2016-10-12 20:07:50 +0100

    update of squadFragment

--- app/src/main/java/com/matty_christopher/englandplayertracker/ScreenSlidePageFragment.java
+++ app/src/main/java/com/matty_christopher/englandplayertracker/ScreenSlidePageFragment.java
@@ -21,16 +21,27 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class ScreenSlidePageFragment extends Fragment {
-    public static final String ARG_PAGE = "page";
-    private int mPageNumber;
-    private static JSONArray data;
+    private JSONArray data;
     private TableLayout tableLayout;
-    private static int[] logo;
     private LayoutInflater inflate;
-    private static ArrayList<String> wins;
-    private static ArrayList<String> draws;
-    private static ArrayList<String> losses;
-    private static ArrayList<String> teams;
+    private String wins;
+    private String draws;
+    private String losses;
+    private String teams;
+    private int logo;
+
+    public static ScreenSlidePageFragment newInstance(int logos,String win,String draw,String loss,String team,String output) {
+        ScreenSlidePageFragment fragmentFirst = new ScreenSlidePageFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("logo", logos);
+        bundle.putString("wins", win);
+        bundle.putString("draws", draw);
+        bundle.putString("losses", loss);
+        bundle.putString("teams",team);
+        bundle.putString("data", output);
+        fragmentFirst.setArguments(bundle);
+        return fragmentFirst;
+    }
 
 
     public ScreenSlidePageFragment() {
@@ -39,13 +50,17 @@ public class ScreenSlidePageFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //mPageNumber = getArguments().getInt("ARG_PAGE");
-        mPageNumber=getArguments().getInt("page");
-        logo = getArguments().getIntArray("logo");
-        wins= getArguments().getStringArrayList("wins");
-        draws= getArguments().getStringArrayList("draws");
-        losses= getArguments().getStringArrayList("losses");
-        teams= getArguments().getStringArrayList("teams");
+        logo = getArguments().getInt("logo");
+        wins= getArguments().getString("wins");
+        draws= getArguments().getString("draws");
+        losses= getArguments().getString("losses");
+        teams= getArguments().getString("teams");
+        String stringData= getArguments().getString("data");
+        try {
+            data=new JSONArray(stringData);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -55,41 +70,76 @@ public class ScreenSlidePageFragment extends Fragment {
         tableLayout=(TableLayout)rootView.findViewById(R.id.squads_tables);
         inflate=inflater;
         fillTable();
-        Log.e("check pos inner", "" + mPageNumber);
 
         return rootView;
     }
 
     private void fillTable() {
-        if (teams.size() > 0) {
+        if (teams.length() > 0) {
 
             View tr = inflate.inflate(R.layout.squadrow, null,false);
             ImageView teamLogo=(ImageView)tr.findViewById(R.id.imageView);
-            Drawable logoDraw= ContextCompat.getDrawable(getActivity(), logo[mPageNumber]);
+            Drawable logoDraw= ContextCompat.getDrawable(getActivity(), logo);
             teamLogo.setImageDrawable(logoDraw);
             TextView teamname=(TextView)tr.findViewById(R.id.name);
-            teamname.setText(teams.get(mPageNumber ));
+            teamname.setText(teams);
             TextView record=(TextView)tr.findViewById(R.id.textView3);
-            record.setText("wins: " + wins.get(mPageNumber ) + "  draws: " + draws.get(mPageNumber ) + "  lost: " + losses.get(mPageNumber ));
+            record.setText("wins: " + wins + "  draws: " + draws + "  lost: " + losses);
             tableLayout.addView(tr);
 
+            try {
+                int currPos=0;
+                boolean passed=false;
+                playerloop:
+                for(int j=0;j<data.length();j++,currPos++){
+                    JSONObject jsonobject = data.getJSONObject(j);
+                    String currTeamName=jsonobject.getString("name");
+                    String playerName=jsonobject.getString("Name");
+                    String position=jsonobject.getString("position");
+                    int age=jsonobject.optInt("Age", 0);
+                    int played=jsonobject.optInt("played", 0);
+                    int goals=jsonobject.optInt("goals", 0);
+                    if(!currTeamName.equals(teams) && passed){
 
-        }
+                        break playerloop;
+                    }
+                    else if(currTeamName.equals(teams) && !playerName.equals("null")){
+                        if(!passed){
+                            passed=true;
+                        }
 
-        for (int i = 0; i < 20; i++) {
-            TableRow tr1 = new TableRow(getActivity());
-            TextView txt = new TextView(getActivity());
-            txt.setText("" + i);
-            tr1.addView(txt);
-            tr1.setMinimumHeight(200);
-            if (i % 2 == 0) {
-                tr1.setBackgroundColor(Color.rgb(226, 244, 66));
+
+
+                        View tr_player = inflate.inflate(R.layout.player_row, null,false);
+                        TextView currPlayer=(TextView)tr_player.findViewById(R.id.name_txt);
+                        currPlayer.setText(playerName);
+
+                        TextView currPosiotion=(TextView)tr_player.findViewById(R.id.textView2);
+                        currPosiotion.setText(position);
+
+                        TextView currAge=(TextView)tr_player.findViewById(R.id.agetxt);
+                        currAge.setText("Age: "+age);
+
+                        TextView currMatches=(TextView)tr_player.findViewById(R.id.matchesTxt);
+                        currMatches.setText("Matches: "+played);
+
+                        TextView currGoals=(TextView)tr_player.findViewById(R.id.textView4);
+                        currGoals.setText("Goals: "+goals);
+
+                        tableLayout.addView(tr_player);
+                    }
+
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            tableLayout.addView(tr1);
+
         }
+
+
+
+
     }
 
-    public int getPageNumber() {
-        return mPageNumber;
-    }
 }
--- app/src/main/java/com/matty_christopher/englandplayertracker/SquadFragment.java
+++ app/src/main/java/com/matty_christopher/englandplayertracker/SquadFragment.java
@@ -5,14 +5,20 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -34,115 +40,81 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.zip.Inflater;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SquadFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link SquadFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SquadFragment extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final int NUM_PAGES = 20;
+    private ViewPager mPager;
+    private ScreenSlidePagerAdapter mPagerAdapter;
+    private JSONArray output;
+    private int[] logos;
+    private ArrayList<String> wins;
+    private ArrayList<String> draws;
+    private ArrayList<String> losses;
+    private ArrayList<String> teams;
     private View v;
-    private LayoutInflater inflate;
-
     private OnFragmentInteractionListener mListener;
+    private AutoCompleteTextView searchbox;
 
     public SquadFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SquadFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SquadFragment newInstance(String param1, String param2) {
-        SquadFragment fragment = new SquadFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        this.inflate=inflater;
-        v=inflater.inflate(R.layout.fragment_squad, container, false);
-
-        EditText searchTeam=(EditText)v.findViewById(R.id.clubEditBox);
+        v= inflater.inflate(R.layout.activity_screen_slide, container, false);
         Button button=(Button)v.findViewById(R.id.button);
         button.setOnClickListener(this);
-
         DatabaseConnection_Fixtures databaseConnection_fixtures=new DatabaseConnection_Fixtures();
         databaseConnection_fixtures.getSquads(new Db_response<JSONArray>() {
             @Override
             public void processFinish(JSONArray output) {
-                if(output.length()>0){
-                    fillTable(output);
-                }
-                else{
+                if (output.length() > 0) {
+                    getData(output);
+                } else {
                     Toast.makeText(getActivity(), "Problem retrieving data", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+
+
+
+
         return v;
     }
 
-    private void fillTable(JSONArray output) {
-        TableLayout tableLayout=(TableLayout)v.findViewById(R.id.squads_tables);
+    private void getData(JSONArray output) {
+        this.output=output;
 
-        int[] logos={R.drawable.afc_bournemouth,R.drawable.arsenal,R.drawable.burnley,R.drawable.chelsea,R.drawable.crystal_palace,
-        R.drawable.everton,R.drawable.hull_city,R.drawable.leicester,R.drawable.liverpool,R.drawable.manchester_city,R.drawable.manchester_united,
-        R.drawable.middlesbrough,R.drawable.southampton,R.drawable.stoke,R.drawable.sunderland,R.drawable.swansea,R.drawable.tottenham,R.drawable.watford,
-        R.drawable.west_bromwich,R.drawable.west_ham};
-
-        ArrayList<String> wins=new ArrayList<>();
-        ArrayList<String> draws=new ArrayList<>();
-        ArrayList<String> losses=new ArrayList<>();
-        ArrayList<String> teams=new ArrayList<>();
-        for (int i=0;i<output.length();i++){
-            String teamName="";
-            String win="";
-            String draw="";
-            String lost="";
+        wins = new ArrayList<>();
+        draws = new ArrayList<>();
+        losses = new ArrayList<>();
+        teams = new ArrayList<>();
+        long time1=new Date().getTime();
+        for (int i = 0; i < output.length(); i++) {
+            String teamName = "";
+            String win = "";
+            String draw = "";
+            String lost = "";
             try {
                 JSONObject jsonobject = output.getJSONObject(i);
-                teamName=jsonobject.getString("name");
-                win=jsonobject.getString("wins");
-                draw=jsonobject.getString("draws");
-                lost=jsonobject.getString("lost");
+                teamName = jsonobject.getString("name");
+                win = jsonobject.getString("wins");
+                draw = jsonobject.getString("draws");
+                lost = jsonobject.getString("lost");
                 //Log.e("test team name",teamName);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            if(!teams.contains(teamName)){
+            if (!teams.contains(teamName)) {
                 teams.add(teamName);
                 wins.add(win);
                 draws.add(draw);
@@ -150,74 +122,66 @@ public class SquadFragment extends Fragment implements View.OnClickListener {
             }
 
         }
-        int loops=0;
-        int startPos=0;
-        long time1=new Date().getTime();
-        for(int i=0;i<logos.length;i++){
-
-            View tr = inflate.inflate(R.layout.squadrow, null,false);
-            ImageView teamLogo=(ImageView)tr.findViewById(R.id.imageView);
-            Drawable logo= ContextCompat.getDrawable(getActivity(),logos[i]);
-            teamLogo.setImageDrawable(logo);
-            TextView teamname=(TextView)tr.findViewById(R.id.name);
-            teamname.setText(teams.get(i));
-            TextView record=(TextView)tr.findViewById(R.id.textView3);
-            record.setText("wins: " + wins.get(i) + "  draws: " + draws.get(i) + "  lost: " + losses.get(i));
-            tableLayout.addView(tr);
-
-            try {
-                int currPos=0;
-                playerloop:
-                for(int j=startPos;j<output.length();j++,currPos++){
-                    JSONObject jsonobject = output.getJSONObject(j);
-                    String currTeamName=jsonobject.getString("name");
-                    String playerName=jsonobject.getString("Name");
-                    String position=jsonobject.getString("position");
-                    int age=jsonobject.optInt("Age", 0);
-                    int played=jsonobject.optInt("played", 0);
-                    int goals=jsonobject.optInt("goals", 0);
-                    loops++;
-                    if(!currTeamName.equals(teams.get(i))){
-                        currPos=j;
-                        startPos=currPos;
-                        //Log.e("check row", currTeamName + " " + teams.get(i) + " " + startPos+" "+j);
-                        break playerloop;
-                    }
-                    else if(!playerName.equals("null")){
-
-
-
-
-                        View tr_player = inflate.inflate(R.layout.player_row, null,false);
-                        TextView currPlayer=(TextView)tr_player.findViewById(R.id.name_txt);
-                        currPlayer.setText(playerName);
-
-                        TextView currPosiotion=(TextView)tr_player.findViewById(R.id.textView2);
-                        currPosiotion.setText(position);
-
-                        TextView currAge=(TextView)tr_player.findViewById(R.id.agetxt);
-                        currAge.setText("Age: "+age);
-
-                        TextView currMatches=(TextView)tr_player.findViewById(R.id.matchesTxt);
-                        currMatches.setText("Matches: "+played);
-
-                        TextView currGoals=(TextView)tr_player.findViewById(R.id.textView4);
-                        currGoals.setText("Goals: "+goals);
-
-                        tableLayout.addView(tr_player);
-                    }
-
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-
-        }
         long time2=new Date().getTime();
-        Log.e("measure Time",""+(time2-time1)+" loops: "+loops);
+        Log.e("measure Time", "" + (time2 - time1) + " loops: ");
+        logos= new int[]{R.drawable.afc_bournemouth, R.drawable.arsenal, R.drawable.burnley, R.drawable.chelsea, R.drawable.crystal_palace,
+                R.drawable.everton, R.drawable.hull_city, R.drawable.leicester, R.drawable.liverpool, R.drawable.manchester_city, R.drawable.manchester_united,
+                R.drawable.middlesbrough, R.drawable.southampton, R.drawable.stoke, R.drawable.sunderland, R.drawable.swansea, R.drawable.tottenham, R.drawable.watford,
+                R.drawable.west_bromwich, R.drawable.west_ham};
+
+
+        searchbox=(AutoCompleteTextView)v.findViewById(R.id.searchbox);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,teams);
+        searchbox.setAdapter(adapter);
+        searchbox.setThreshold(1);
+        searchbox.setHint("enter team here...");
+
+        mPager = (ViewPager) v.findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getChildFragmentManager());
+
+        new setAdapterTask().execute();
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                gotoPage();
+                break;
+        }
+    }
+
+    private void gotoPage() {
+        String word=searchbox.getText().toString();
+        int pos=-1;
+        for(int i=0;i<teams.size();i++){
+            if(teams.get(i).equalsIgnoreCase(word)){
+                pos=i;
+                break;
+            }
+        }
+        if(pos>=0){
+            Toast.makeText(getActivity(), word, Toast.LENGTH_SHORT).show();
+            mPager.setCurrentItem(pos);
+            searchbox.setText("");
+        }
+        else{
+            Toast.makeText(getActivity(), "Could not find team", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class setAdapterTask extends AsyncTask<Void,Void,Void> {
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            mPager.setAdapter(mPagerAdapter);
+            mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        }
+    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -242,27 +206,38 @@ public class SquadFragment extends Fragment implements View.OnClickListener {
         mListener = null;
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button:
-                Toast.makeText(getActivity(), "Works", Toast.LENGTH_SHORT).show();
-                break;
-        }
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+
+        }
+
+        @Override
+        public ScreenSlidePageFragment getItem(int position) {
+            Bundle bundle = new Bundle();
+
+            int logo=logos[position];
+            String win=wins.get(position);
+            String draw=draws.get(position);
+            String loss=losses.get(position);
+            String team=teams.get(position);
+            String data=output.toString();
+
+            Log.e("check pos outer", "" + position+" "+teams.get(position));
+
+            return ScreenSlidePageFragment.newInstance(logo,win,draw,loss,team,data);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
